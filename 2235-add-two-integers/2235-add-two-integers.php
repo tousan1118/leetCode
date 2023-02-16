95dc7395aa1500e5275ffc704db1bff4fe83d115
tousan1118 <102907729+tousan1118@users.noreply.github.com>
2023-02-16 13:02:29 +0900

    Time: 12 ms (32.68%), Space: 19.3 MB (34.24%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.php
@@ -0,0 +1,11 @@
+class Solution {
+
+    /**
+     * @param Integer $num1
+     * @param Integer $num2
+     * @return Integer
+     */
+    function sum($num1, $num2) {
+        return (int)$num1 + (int)$num2;
+    }
+}
