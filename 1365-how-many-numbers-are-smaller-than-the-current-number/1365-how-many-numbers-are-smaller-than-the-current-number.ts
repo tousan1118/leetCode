b707a628dcd118812cd1d32f10c7e77296902744
tousan1118 <102907729+tousan1118@users.noreply.github.com>
2023-02-26 23:12:52 +0900

    Time: 80 ms (87.79%), Space: 44.7 MB (82.56%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.ts
@@ -0,0 +1,5 @@
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+  const sorted = Array.from(nums).sort((n1, n2) => n2 - n1);
+  const map = new Map(sorted.map((num, index) => [num, nums.length - index - 1]));
+  return nums.map(num => map.get(num));
+};      
